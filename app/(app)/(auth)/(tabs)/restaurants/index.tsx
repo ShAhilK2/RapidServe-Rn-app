200e27cfece0c07918bbc253efdd994ca0a0c2c6
ShAhilK2 <shahilkataria852002@gmail.com>
2025-11-26 16:53:29 +0530

    feat : Categories and restaurants

--- app/(app)/(auth)/(tabs)/restaurants/index.tsx
+++ app/(app)/(auth)/(tabs)/restaurants/index.tsx
@@ -1,14 +1,45 @@
+import { CategoryList } from "@/components/screens/CategoryList";
+import RestaurantList from "@/components/screens/RestaurantList";
+import { Fonts } from "@/constants/theme";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Animated from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const Page = () => {
+  const insets = useSafeAreaInsets();
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={styles.container}>
+      <Animated.ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingTop: insets.top + 60 }}
+      >
+        <Text style={styles.pageTitle}>Restaurant</Text>
+        <CategoryList />
+
+        <Text style={styles.allRestaurantsTitle}>All restaurants</Text>
+        <RestaurantList />
+      </Animated.ScrollView>
     </View>
   );
 };
 
 export default Page;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  pageTitle: {
+    fontFamily: Fonts.brandBlack,
+    fontSize: 30,
+    marginBottom: 16,
+    paddingHorizontal: 16,
+  },
+  allRestaurantsTitle: {
+    fontFamily: Fonts.brandBold,
+    fontSize: 20,
+    marginBottom: 16,
+    paddingHorizontal: 16,
+  },
+});
